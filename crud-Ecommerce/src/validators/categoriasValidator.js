48ebd5671ca0a78de0c03bd8e1bde3fe57d5ed6f
yuriribeiro-ctrl <yuri.ribeiro@iesb.edu.br>
2025-11-14 20:41:13 -0300

    carrinhovalidator

---
+++ crud-Ecommerce/src/validators/categoriasValidator.js
@@ -0,0 +1,17 @@
+const yup = require('yup');
+
+const schema = yup.object().shape({
+nome: yup.string().required("nome é obrigatório"),
+descricao: yup.string().notRequired()
+});
+
+async function validarCategoria(req, res, next) {
+try {
+await schema.validate(req.body, { abortEarly: false });
+next();
+} catch (error) {
+return res.status(400).json({ erros: error.errors });
+}
+}
+
+module.exports = { validarCategoria };
