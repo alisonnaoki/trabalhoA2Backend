12cc9e02c5ee811a948bfe06b8f7fa95824a0f4e
PedroArthur710 <p.arthur@iesb.edu.br>
2025-11-14 20:54:22 -0300

    produtosControl

---
+++ crud-Ecommerce/src/controllers/produtosControl.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+const ProdutoModel = require('../models/produtosModel');
+const { validarProduto } = require('../validators/produtosValidator');
+
+router.get('/produtos', async (req, res) => {
+const produtos = await ProdutoModel.find()
+    .populate('categoriaId');
+    res.json(produtos);
+});
+
+router.get('/produtos/:id', async (req, res) => {
+const produto = await ProdutoModel.findById(req.params.id)
+    .populate('categoriaId');
+if (!produto) return res.status(404).json({ erro: "Não encontrado" });
+    res.json(produto);
+});
+
+router.post('/produtos', validarProduto, async (req, res) => {
+const novoProduto = await ProdutoModel.create(req.body);
+    res.status(201).json(novoProduto);
+});
+
+router.put('/produtos/:id', async (req, res) => {
+const atualizado = await ProdutoModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+if (!atualizado) return res.status(404).json({ erro: "Não encontrado" });
+    res.json(atualizado);
+});
+
+router.delete('/produtos/:id', async (req, res) => {
+await ProdutoModel.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+});
+
+module.exports = router;
+
