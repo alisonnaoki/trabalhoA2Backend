66444d86a6c8d5dd073f7d6fd7e55c87789ee7f2
alisonnaoki <alison.tsuboi@iesb.edu.br>
2025-11-14 20:51:50 -0300

    correcoes

--- crud-Ecommerce/src/controllers/fornecedoresControl.js
+++ crud-Ecommerce/src/controllers/fornecedoresControl.js
@@ -5,30 +5,30 @@ const FornecedorModel = require('../models/fornecedoresModel');
 const { validarFornecedor } = require('../validators/fornecedoresValidator');
 
 router.get('/fornecedores', async (req, res) => {
-const fornecedores = await FornecedorModel.find().populate('produtosFornecidos');
-res.json(fornecedores);
+    const fornecedores = await FornecedorModel.find().populate('produtosFornecidos');
+    res.json(fornecedores);
 });
 
 router.get('/fornecedores/:id', async (req, res) => {
-const fornecedor = await FornecedorModel.findById(req.params.id).populate('produtosFornecidos');
-if (!fornecedor) return res.status(404).json({ erro: "Não encontrado" });
-res.json(fornecedor);
+    const fornecedor = await FornecedorModel.findById(req.params.id).populate('produtosFornecidos');
+        if (!fornecedor) return res.status(404).json({ erro: "Não encontrado" });
+    res.json(fornecedor);
 });
 
 router.post('/fornecedores', validarFornecedor, async (req, res) => {
-const novoFornecedor = await FornecedorModel.create(req.body);
-res.status(201).json(novoFornecedor);
+    const novoFornecedor = await FornecedorModel.create(req.body);
+    res.status(201).json(novoFornecedor);
 });
 
 router.put('/fornecedores/:id', async (req, res) => {
-const atualizado = await FornecedorModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
-if (!atualizado) return res.status(404).json({ erro: "Não encontrado" });
-res.json(atualizado);
+    const atualizado = await FornecedorModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!atualizado) return res.status(404).json({ erro: "Não encontrado" });
+    res.json(atualizado);
 });
 
 router.delete('/fornecedores/:id', async (req, res) => {
-await FornecedorModel.findByIdAndDelete(req.params.id);
-res.status(204).send();
+    await FornecedorModel.findByIdAndDelete(req.params.id);
+    res.status(204).send();
 });
 
 module.exports = router;
