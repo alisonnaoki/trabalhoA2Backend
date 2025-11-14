0fd2b44ea8302d3548e8bb4176d7f852dae4eccd
PedroArthur710 <p.arthur@iesb.edu.br>
2025-11-14 20:52:21 -0300

    correçao

--- crud-Ecommerce/src/validators/cupomValidator.js
+++ crud-Ecommerce/src/validators/cupomValidator.js
@@ -1,10 +1,10 @@
 const yup = require('yup');
 
 const schema = yup.object().shape({
-codigo: yup.string().required("codigo é obrigatório"),
-desconto: yup.number().min(0, "desconto mínimo é 0").max(100, "desconto máximo é 100").required("desconto é obrigatório"),
-validade: yup.date().min(new Date(), "validade deve ser futura").required("validade é obrigatória"),
-clienteId: yup.string().length(24).notRequired()
+    codigo: yup.string().required("codigo é obrigatório"),
+    desconto: yup.number().min(0, "desconto mínimo é 0").max(100, "desconto máximo é 100").required("desconto é obrigatório"),
+    validade: yup.date().min(new Date(), "validade deve ser futura").required("validade é obrigatória"),
+    clienteId: yup.string().length(24).notRequired()
 });
 
 async function validarCupom(req, res, next) {
